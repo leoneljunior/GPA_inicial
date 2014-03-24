3a692cf860552410cc765f573e0d0b366cc1c518
salomaosilvasantos <salomaosilvasantos@gmail.com>
2014-03-24 15:01:41 -0300

    Adicionei o metodo redInserir no ContatoController , que redireciona para a página contato/inserir.jsp que chama outro metodo novo que é o inserirContato.

--- src/main/java/br/ufc/quixada/npi/controller/ContatoController.java
+++ src/main/java/br/ufc/quixada/npi/controller/ContatoController.java
@@ -15,17 +15,28 @@ import br.ufc.quixada.npi.service.ContatoService;
 public class ContatoController {
 	@Inject
 	private ContatoService cs;
- 
-	@RequestMapping(value="/", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String listarContatos(ModelMap model) {
 		model.addAttribute("contatos", cs.findAll());
 		return "contato/listar_contatos";
 	}
 
-	@RequestMapping(value="/hello", method = RequestMethod.GET)
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello(ModelMap model) {
 		model.addAttribute("nome", "João");
 		return "contato/ola";
+	}
+	
+	@RequestMapping(value = "/inserirContato", method = RequestMethod.GET)
+	public String redInserir(){
+		return "contato/inserir";
+	}
+	
+	@RequestMapping(value = "/inserirContato", method = RequestMethod.GET)
+	public String inserirContato(Contato contato) {
+		cs.insere(contato);
+		return "contato/listar_contatos";
 	}
 
 }
